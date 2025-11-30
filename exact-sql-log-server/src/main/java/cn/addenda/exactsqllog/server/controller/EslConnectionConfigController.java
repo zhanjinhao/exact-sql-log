c9e0ff05d8020d984fbc48970cc0ae594b1f5204
addenda <isjinhao@163.com>
2025-11-30 18:53:11 +0800

    移除base-component

--- exact-sql-log-server/src/main/java/cn/addenda/exactsqllog/server/controller/EslConnectionConfigController.java
+++ exact-sql-log-server/src/main/java/cn/addenda/exactsqllog/server/controller/EslConnectionConfigController.java
@@ -1,7 +1,7 @@
 package cn.addenda.exactsqllog.server.controller;
 
-import cn.addenda.component.base.jackson.util.JacksonUtils;
 import cn.addenda.exactsqllog.common.config.EslConnectionConfig;
+import cn.addenda.exactsqllog.ext.json.EslJsonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +15,7 @@ public class EslConnectionConfigController {
 
   @PostMapping("receiveEslConnectionConfig")
   public void receiveExecution(@RequestBody EslConnectionConfig eslConnectionConfig) {
-    log.info("{}", JacksonUtils.toStr(eslConnectionConfig));
+    log.info("{}", EslJsonUtils.toStr(eslConnectionConfig));
   }
 
 }
--- exact-sql-log-server/src/main/java/cn/addenda/exactsqllog/server/controller/ExecutionController.java
+++ exact-sql-log-server/src/main/java/cn/addenda/exactsqllog/server/controller/ExecutionController.java
@@ -1,8 +1,8 @@
 package cn.addenda.exactsqllog.server.controller;
 
-import cn.addenda.component.base.jackson.util.JacksonUtils;
 import cn.addenda.exactsqllog.common.bo.PreparedSqlBo;
 import cn.addenda.exactsqllog.common.bo.SqlBo;
+import cn.addenda.exactsqllog.ext.json.EslJsonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +16,12 @@ public class ExecutionController {
 
   @PostMapping("receivePreparedSqlBo")
   public void receivePreparedSqlBo(@RequestBody PreparedSqlBo preparedSqlBo) {
-    log.info("{}", JacksonUtils.toStr(preparedSqlBo));
+    log.info("{}", EslJsonUtils.toStr(preparedSqlBo));
   }
 
   @PostMapping("receiveSqlBo")
   public void receiveSqlBo(@RequestBody SqlBo sqlBo) {
-    log.info("{}", JacksonUtils.toStr(sqlBo));
+    log.info("{}", EslJsonUtils.toStr(sqlBo));
   }
 
 }
